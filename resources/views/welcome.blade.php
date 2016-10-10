47e2e22bff5fb3ca49acf148346af3dc2c22b3c3
kashley <kashley@cs.odu.edu>
2016-10-10 12:05:15 -0400

    Change title of Main Page on master branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">CS411 Orange Team</div>
+                <div class="title">ResearchLink</div>
             </div>
         </div>
     </body>
