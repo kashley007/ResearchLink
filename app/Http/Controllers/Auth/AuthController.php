7ca422a8a4522f91944b1c21bb922bc58f642b91
mredeniu <mredeniu@cs.odu.edu>
2016-11-07 19:16:33 -0500

    added the ability to select user type to register form

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,13 +1,18 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
-
 use App\User;
 use Validator;
+use Mail;
+use App\Notifications;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\profileHelpers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
-
 class AuthController extends Controller
 {
     /*
@@ -20,16 +25,13 @@ class AuthController extends Controller
     | a simple trait to add these behaviors. Why don't you explore it?
     |
     */
-
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-
     /**
      * Where to redirect users after login / registration.
      *
      * @var string
      */
-    protected $redirectTo = '/';
-
+    protected $redirectTo = '/home';
     /**
      * Create a new authentication controller instance.
      *
@@ -39,7 +41,6 @@ class AuthController extends Controller
     {
         $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
     }
-
     /**
      * Get a validator for an incoming registration request.
      *
@@ -53,9 +54,11 @@ class AuthController extends Controller
             'last_name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
+            'user_type' => 'required'
+        ], [
+            'user_type.required' => 'User type is required'
         ]);
     }
-
     /**
      * Create a new user instance after a valid registration.
      *
@@ -64,11 +67,112 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $confirmation_code = str_random(50);
+       
+        User::create([
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'confirmation_code' => $confirmation_code,
+            'confirmed' => 0
         ]);
+        
+        $userData = array('first_name' => $data['first_name'], 'email' => $data['email'], 'confirmation_code' => $confirmation_code );
+        Mail::send('auth.emails.verify', ['userData' => $userData], function ($message) use ($userData) {
+            $message->to($userData['email'], $userData['first_name'])
+               ->subject('Verify your email address');
+        });
+        unset( $userData );
+    }
+    /**
+     * Register a new user instance after a valid registration.
+     *
+     * @param  $request
+     * @return redirect to login screen
+     */
+    public function register(Request $request)
+    {
+        
+        $validator = $this->validator($request->all());
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                $request, $validator
+            );
+        }
+        
+        $this->create($request->all());
+        $user = User::whereEmail($request->email)->first();
+        $user->profile->user_type = $request->user_type;
+        $user->profile->save();
+        Session::flash('message', 'Thank you for signing up! Please check your email to verify your account'); 
+        return Redirect::to('login');
+    }
+    /**
+     * Confrim a new user instance after user clicks on confirmation email link.
+     *
+     * @param  $request
+     * @return redirect to login screen
+     */
+    public function confirm($confirmation_code)
+    {
+        if( ! $confirmation_code)
+        {
+            throw new InvalidConfirmationCodeException;
+        }
+        $user = User::whereConfirmationCode($confirmation_code)->first();
+        if ( ! $user)
+        {
+            throw new InvalidConfirmationCodeException;
+        }
+        $user->confirmed = 1;
+        $user->confirmation_code = null;
+        //Create the welcome notification
+        
+        $notification = new Notifications;
+        $notification->user_id = $user->id;
+        $notification->type_of_notification = 'welcome';
+        $notification->title_html = 'Welcome';
+        $notification->body_html = 'Welcome to ResearchLink! Remember to complete your profile to get linked to new research opportunities. Thank you for signing up and good luck!';
+        $notification->is_read = 0;
+        $notification->save();
+        $user->save();
+        Session::flash('message', 'Thank you for verifying your account.'); 
+        return Redirect::to('login');
+    }
+     /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $user = User::whereEmail($request->email)->first();
+        if($user->confirmed == 0){
+            Session::flash('message', 'Please verify your account before logging in'); 
+            return Redirect::to('login')
+                            ->withInput();
+        }
+        $this->validateLogin($request);
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        $throttles = $this->isUsingThrottlesLoginsTrait();
+        if ($throttles && $lockedOut = $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+        $credentials = $this->getCredentials($request);
+        if (Auth::guard($this->getGuard())->attempt($credentials, $request->has('remember'))) {
+            return $this->handleUserWasAuthenticated($request, $throttles);
+        }
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        if ($throttles && ! $lockedOut) {
+            $this->incrementLoginAttempts($request);
+        }
+        return $this->sendFailedLoginResponse($request);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,17 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row guestPass">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
                             <label for="first_name" class="col-md-4 control-label">First Name</label>
-
                             <div class="col-md-6">
                                 <input id="first_name" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
-
                                 @if ($errors->has('first_name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('first_name') }}</strong>
@@ -23,12 +19,10 @@
                                 @endif
                             </div>
                         </div>
-                        <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
+                         <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
                             <label for="last_name" class="col-md-4 control-label">Last Name</label>
-
                             <div class="col-md-6">
                                 <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
-
                                 @if ($errors->has('last_name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('last_name') }}</strong>
@@ -38,10 +32,8 @@
                         </div>
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -49,13 +41,10 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
-
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password">
-
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -63,13 +52,10 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
-
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
@@ -77,10 +63,35 @@
                                 @endif
                             </div>
                         </div>
-
+                        <div class="form-group{{ $errors->has('user_type') ? ' has-error' : '' }}">
+                            <label for="user_type" class="col-md-4 control-label">User Type:</label>
+                            <div class="col-md-6" id="userClick">
+                                <ul>
+                                    <li>  
+                                        <input id="user_type1" type="radio" class="form-control" name="user_type" value="Student">
+                                        <label for="user_type1">
+                                            <img src="{{ URL::asset('/images/student.png') }}" class="img-square" alt="Student">
+                                        </label>
+                                        <h4>Student</h4>
+                                    </li>
+                                    <li>  
+                                        <input id="user_type2" type="radio" class="form-control" name="user_type" value="Faculty">
+                                        <label for="user_type2">
+                                            <img src="{{ URL::asset('/images/faculty.png') }}" class="img-square" alt="student">
+                                        </label>
+                                        <h4>Faculty</h4>
+                                    </li>
+                                </ul>
+                                @if ($errors->has('user_type'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('user_type') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button id="registerbtn" type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-user"></i> Register
                                 </button>
                             </div>
